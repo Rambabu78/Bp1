ec8369bd5f5e914677898e8b6b50fa1bb83b501d
chaithanya8565 <chaithanya8565gmail.com>
2018-06-12 17:15:13 +0530

    second commit

--- name.java
+++ name.java
@@ -18,22 +18,7 @@ public class dtp {
 
 		            try
 		            {
-		                 long x=sc.nextLong();
-		                System.out.println(x+" can be fitted in:");
-		                if(x>=-((int)Math.pow(2,7))&&x<=((int)Math.pow(2,7)-1))
-		                   {
-		                    System.out.println("* byte");
-		                   }
-		                if(x>=-((int)Math.pow(2,15))&&x<=((int)Math.pow(2,15)-1))
-		                   {
-		                    System.out.println("* short");
-		                   }
-		                if(x>=-((int)Math.pow(2,31))&&x<=((int)Math.pow(2,31)-1))
-		                   {
-		                    System.out.println("* int");
-		                   }
-		                if(x>=-((long)Math.pow(2,63))&&x<=((long)Math.pow(2,63))-1)
-		                   {
+		              
 		                    
 		                    System.out.println("* long");
 		                   }
